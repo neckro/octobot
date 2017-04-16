6f8d59f4db02fe2652542aedc41d3be1d50cdede
Joseph Culbert <joseph@culbert.mn>
2017-04-16 18:26:15 -0500

    Node v6: Convert prototypes to ES6 classes; add eslint

--- example.js
+++ example.js
@@ -1,26 +1,27 @@
 #!/usr/bin/env node
 
-var Octobot = require('./lib');
-var bot = new Octobot();
+const Octobot = require('./lib');
+const bot = new Octobot();
 
-bot
-.load('logger', {
-  logdir: './logs',
-  logfiles: {
-    'octobot.log': {
-      levels: ['debug', 'irc', 'log']
+(bot
+  .load('logger', {
+    logdir: './logs',
+    logfiles: {
+      'octobot.log': {
+        levels: ['debug', 'irc', 'log']
+      }
     }
-  }
-})
-.load('main')
-.load('irc', {
-  nick: 'Octobot',
-  userName: 'octobot',
-  realName: 'Octobot',
-  port: 6667,
-  channels: ['#octotest'],
-  server: 'irc.lunarnet.org'
-})
-.load('database')
-.load('dictionary')
-.start();
+  })
+  .load('main')
+  .load('irc', {
+    nick: 'Octobot',
+    userName: 'octobot',
+    realName: 'Octobot',
+    port: 6667,
+    channels: ['#octotest'],
+    server: 'irc.lunarnet.org'
+  })
+  .load('database')
+  .load('dictionary')
+  .start()
+);
